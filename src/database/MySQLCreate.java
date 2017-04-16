377d7af82df8026ebc08b8e80d817b797ccb8c10
Justin Wang <jusnit006@gmail.com>
2017-04-16 18:19:15 +0800

    database create

---
+++ src/database/MySQLCreate.java
@@ -0,0 +1,86 @@
+package database;
+
+import java.sql.*;
+
+public class MySQLCreate {
+   // JDBC driver name and database URL
+   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   static final String DB_URL = "jdbc:mysql://localhost/";
+
+   //  Database credentials
+   static final String USER = "username";
+   static final String PASS = "password";
+   
+   public MySQLCreate(){
+	   try{
+		   Class.forName("com.mysql.jdbc.Driver");
+	   }catch(Exception e){}
+   }
+   
+   public void create(String s){
+	   Connection conn = null;
+	   Statement st = null;
+	   try{
+		   conn = DriverManager.getConnection(DB_URL);
+		   System.out.println("Connecting....");
+		   st = conn.createStatement();
+		   String sql = "CREATE DATABASE STORAGE";
+		   st.executeUpdate(sql);
+		   System.out.println("Database Create succesfully....");
+		   
+	   }catch(Exception e){
+		   
+	   }finally{
+		   try{
+			   if(st != null)
+				   st.close();
+		   }catch(Exception e){}
+		   try{
+			   if(conn != null)
+				   conn.close();
+		   }catch(Exception e){}
+		   System.out.println("closed connection....");
+	   }
+   }
+   
+   public static void main(String[] args) {
+   Connection conn = null;
+   Statement stmt = null;
+   try{
+      //STEP 2: Register JDBC driver
+      Class.forName("com.mysql.jdbc.Driver");
+
+      //STEP 3: Open a connection
+      System.out.println("Connecting to database...");
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+      //STEP 4: Execute a query
+      System.out.println("Creating database...");
+      stmt = conn.createStatement();
+      
+      String sql = "CREATE DATABASE STUDENTS";
+      stmt.executeUpdate(sql);
+      System.out.println("Database created successfully...");
+   }catch(SQLException se){
+      //Handle errors for JDBC
+      se.printStackTrace();
+   }catch(Exception e){
+      //Handle errors for Class.forName
+      e.printStackTrace();
+   }finally{
+      //finally block used to close resources
+      try{
+         if(stmt!=null)
+            stmt.close();
+      }catch(SQLException se2){
+      }// nothing we can do
+      try{
+         if(conn!=null)
+            conn.close();
+      }catch(SQLException se){
+         se.printStackTrace();
+      }//end finally try
+   }//end try
+   System.out.println("Goodbye!");
+}//end main
+}//end JDBCExample
--- src/resources/Controller1.java
+++ src/resources/Controller1.java
@@ -25,37 +25,12 @@ public class Controller1 implements Initializable{
 		btn1.setOnMouseClicked(new EventHandler(){
 			@Override
 			public void handle(Event arg0) {
-//				FXMLLoader loader= new FXMLLoader();
 				FXMLLoader loader2 = new FXMLLoader();
-//				loader.setLocation(this.getClass().getResource("/application/grid.fxml"));
 				loader2.setLocation(this.getClass().getResource("/application/btn1_subpage.fxml"));
-//				BorderPane bp = null;
-//				GridPane gp1 = null;
-//				GridPane gp2 = null;
 				AnchorPane ap = null;
 				try{
-//					gp1 = (GridPane)loader.load();
-//					gp2 = (GridPane)gp1.lookup("#grid_main_page");
-//					bp = (BorderPane)gp2.lookup("#main_content");
 					ap = (AnchorPane)loader2.load();
-//					if(ap == null)System.out.println("ap is null");
-//					if(gp1 == null) System.out.println("gp1 is null");
-//					if(gp2 == null)System.out.println("gp2 is  null");
 				}catch(Exception e){System.out.println(e.getMessage()+",Holy") ;}
-//				System.out.println("click");l
-//				bp.getChildren().add(new Button("123"));
-//				Button  b = new Button("NEW BUTTON");
-//				b.setMaxHeight(Double.MAX_VALUE);
-//				b.setMaxWidth(Double.MAX_VALUE);
-//				b.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-//				main_content.add(new Button("123"),1,1);
-//				ap.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-//				main_content.getChildren().clear();
-//				main_content.setLeftAnchor(ap, 0d);
-//				main_content.setTopAnchor(ap, 0d);
-//				main_content.setRightAnchor(ap, 0d);
-//				main_content.setBottomAnchor(ap, 0d);
-//				main_content.getChildren().add(ap);
 				setMainContent(main_content, ap);
 			}
 			
